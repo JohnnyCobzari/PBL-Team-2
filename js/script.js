ccf05c3feaab72b8ac0dccc4ab91d5d6bbc10caa
Mihai Gurduza <gurduzamihai@gmail.com>
2024-04-04 21:08:25 +0300

    adjusted map size, fov, particle distribution

--- js/script.js
+++ js/script.js
@@ -1,30 +1,41 @@
 import * as THREE from 'three';
+
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 2;
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 200;
+camera.lookAt(scene.position);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+
+let ang_rad = camera.fov * Math.PI / 180;
+let fov_y = camera.position.z * Math.tan(ang_rad / 2) * 2;
 var earthMap = new THREE.TextureLoader().load('../assets/land_ocean_ice_2048.png');
-var earthGeometry = new THREE.PlaneGeometry(4, 2);
+earthMap.minFilter = THREE.LinearFilter;
+var earthGeometry = new THREE.PlaneGeometry(360, 180);
 var earthMaterial = new THREE.MeshBasicMaterial({ map: earthMap, side: THREE.DoubleSide });
 var earth = new THREE.Mesh(earthGeometry, earthMaterial);
 scene.add(earth);
 
-var particleCount = 1000;
+var particleCount = 100000;
 var particles = new THREE.BufferGeometry();
 var positions = new Float32Array(particleCount * 3);
 
+const response = await fetch('../assets/small_wind_data.json');
+const windData = await response.json();
+  
+console.log(windData);
+
 for (var i = 0; i < particleCount; i++) {
-    var x = Math.random() * 4 - 2;
-    var y = Math.random() * 2 - 1;
-    var z = Math.random() * 0.2;
+	var x = Math.random() * 360 - 180;
+    var y = Math.random() * 180 - 90;
+    var z = 0;
 
     positions[i * 3] = x;
     positions[i * 3 + 1] = y;
-    positions[i * 3 + 2] = z;
+    positions[i * 3 + 2] = 0;
 }
 
 particles.setAttribute('position', new THREE.BufferAttribute(positions, 3));
@@ -41,10 +52,10 @@ function animateParticles() {
         var y = positionAttribute.getY(i);
         var z = positionAttribute.getZ(i);
 
-        z -= 0.001; // Adjust the speed of particles
-        if (z < 0) {
-            z = 0.2; // Reset the particles when they move out of view
-        }
+        // x -= 0.001; // Adjust the speed of particles
+        // if (x < 0) {
+        //     x = 0.2; // Reset the particles when they move out of view
+        // }
 
         positionAttribute.setXYZ(i, x, y, z);
     }
